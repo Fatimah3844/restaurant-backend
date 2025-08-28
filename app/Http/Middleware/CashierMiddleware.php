6ba07ac301fdb299eeae0f4758600c8e61b02882
Malak-694 <malakeltabakh2020@gmail.com>
2025-08-28 22:05:16 +0300

    modifing cashier middleware

--- app/Http/Middleware/CashierMiddleware.php
+++ app/Http/Middleware/CashierMiddleware.php
@@ -4,17 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class CashierMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-       
 
-         $user = $request->user;
 
-        if (!$user || $user->role !== 'cashier'|| $user->role !== 'admin') {
-            return response()->json(['message' => 'Unauthorized. Cashiers only.'], 403);
+        $user = $request->user;
+        if (!$user || !($user->role == 'cashier' || $user->role == 'admin')) {
+            return response()->json(['message' => 'Unauthorized. Cashiers or Admin only.'], 403);
         }
 
         return $next($request);
